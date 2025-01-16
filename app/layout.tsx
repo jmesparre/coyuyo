a0128ec82403cbfa3f349439f093987a5ecff5c8
jmesparre <jmesparre@gmail.com>
2025-01-16 09:49:41 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Cursor from '@/components/Cursor'
+import Cursor from '../components/Cursor'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
