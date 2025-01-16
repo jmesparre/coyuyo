f530abd744ecfcfef22a82ced30a9d5e70b5e300
Juan Manuel <jmesparre@gmail.com>
2025-01-15 22:12:42 -0300

    Mensaje descriptivo del commit

---
+++ app/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+    return <h2 className="bg-white p-10 text-center max-h-10 max-w-10">loading</h2>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,80 @@
+"use client"
+
 import Image from "next/image";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react"
+import ParralaxImage from "./ParallaxImage"
+import { ReactLenis } from "@studio-freight/react-lenis"
+
 
 export default function Home() {
+  const boxRef = useRef();
+  useGSAP(() => {
+    gsap.to(boxRef.current, {
+      x: 500,
+      duration: 1,
+    });
+  });
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+            
+    <ReactLenis root>
+   <main className="overflow-hidden">
+      <div className="nav" ref={boxRef}>
+            <p>Home</p>
+            <p>Productos</p>
+            <p>Servicios</p>
+            <a className="view" href="#">Contacto</a>
+      </div>
+      <section className="hero">
+          <div className="ParralaxImage-bg">
+            <ParralaxImage src="bg.svg" alt=""/>
+          </div>   
+          <div className="title col items-center w-full">
+             <img src="log1.svg" alt="" className="logo-t w-full justify-center" />
+             <h1>Coyuyo</h1>
+          </div>
+     </section>    
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <section className="producto">
+          <div className="ParralaxImage-bg ">
+            <ParralaxImage src="botella2.png" alt=""/>
+          </div>
+          <div className="description">
+            <h3>Aceite del copado</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur <s>explicabo </s>deleniti a inventore vel ratione neque est nostrum dolore mollitia! Blanditiis <u> consectetur</u> dolorem fugit quis quo non reprehenderit. Quaerat, non.</p>
+          </div>
+        </section>  
+
+
+        <section className="informacion">
+  
+          <div className="col img-informacion ParralaxImage pt-40">
+            <ParralaxImage src="/cañamo.jpg" alt="" />
+          </div>
+          <div className="intro">
+            <h4>introduccion</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, sequi animi unde necessitatibus numquam laboriosam praesentium possimus, excepturi molestias iste doloribus cum dolorum beatae non quae cumque. Quasi, neque maxime.</p>
+          </div>
+                
+        </section>   
+
+       
+
+
+
+        <section className="footer bg-green-800">
+        
+        <div className="col text-black">
+          <p>instagram</p>
+          <p>instagram</p>
+          <p>instagram</p>
         </div>
+        <div className="col"></div>
+          
+        </section>
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </ReactLenis>
   );
 }
