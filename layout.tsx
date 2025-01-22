60ca241e13d113b8c13efe3a7d4074e18734f81b
jmesparre <jmesparre@gmail.com>
2025-01-22 20:37:42 -0300

    flix @/navbar

--- layout.tsx
+++ layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import NavBar from "../components/NavBar";
+import NavBar from "@/components/NavBar";
 
 export const metadata: Metadata = {
   title: "Coyuyo",
@@ -16,9 +16,6 @@ export default function RootLayout({
     <>
       <html lang="es">
         <head><meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <style>
-         @import url('https://fonts.googleapis.com/css2?family=Corben&display=swap');
-        </style>
         </head>
         <body>
           <NavBar />
