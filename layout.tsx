0bb9bd0504903ab347966a18e353c414cf36cdfe
jmesparre <jmesparre@gmail.com>
2025-01-22 20:41:15 -0300

    fuente corben

--- layout.tsx
+++ layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
     <>
       <html lang="es">
         <head><meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <style>
+      @import url('https://fonts.googleapis.com/css2?family=Corben&display=swap');
+      </style>
         </head>
         <body>
           <NavBar />
